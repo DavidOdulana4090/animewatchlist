bc969e9692d159083f7e5cbba116cc9fe13b3c2b
David <daveedtee5@gmail.com>
2026-02-23 17:31:17 -0500

    Init

---
+++ src/components/button.jsx
@@ -0,0 +1,13 @@
+import '../styles/button.css' 
+import { useState } from 'react';
+
+function button() {
+    const []
+    return (
+        <button>
+
+        </button>
+      );
+}
+
+export default button;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Add the dots and stars!
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
