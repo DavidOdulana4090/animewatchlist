92151bd6a1ff551477b3c9b982b8824f4e247993
David <daveedtee5@gmail.com>
2026-02-25 12:47:09 -0500

    Type fix using main local storage fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,12 @@ import { useState } from "react";
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
-    const [isLoggedIn, SetIsLogstate] = useState(() => {
+    const [isLoggedIn, SetIsLogstate] = useState<string>(() => {
         const saved = localStorage.getItem('isLoggedIn');
         return saved !== null ? saved : 'false';
-        // 
+        //  will focus on enhancing security later only a demo project
     });
 
-    // isLoggedIn = ("true")  || ("false") Localstorage,stateforRendering
     const LoginRequest = () => {
         localStorage.setItem('isLoggedIn', 'true');
         SetIsLogstate('true');
@@ -30,13 +29,16 @@ function App() {
         <>
             <Routes>
                 {/*Whether to render login or home based on local storage saved key = value then State  to update */}
-                <Route path="/home" element={isLoggedIn === "true" ? <HomePage /> : <Navigate to={"/login"} />} /> 
+                <Route path="/home" element={isLoggedIn === 'true' ? <HomePage /> : <Navigate to={"/login"} />} /> 
 
-                <Route  path="/login" element={isLoggedIn ? <Navigate to="/home" /> : <LoginPage loginSuccess={LoginRequest} />} />
+                <Route  path="/login" element={isLoggedIn === 'true' ? <Navigate to="/home" /> : <LoginPage loginSuccess={LoginRequest} />} />
                 
                 <Route path="/sign-up" element={<CreateAccountPage />} />
 
-                <Route path="/home" element={<HomePage />}  />
+                <Route path="/home" element={<HomePage />} />
+                
+                {/* Default Route - Home then redirect Based of States */}
+                <Route path="/" element={<Navigate to="/home" replace />} />
             </Routes>
         </>
     );}
