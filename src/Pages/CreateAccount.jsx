11cbfea291e9202a10675683ab1ac3986851db32
David <daveedtee5@gmail.com>
2026-03-14 12:27:04 -0400

    finished create account page, protected routes work perfectly, using a webp image instead

--- src/Pages/CreateAccount.jsx
+++ src/Pages/CreateAccount.jsx
@@ -1,7 +1,7 @@
 import '../styles/CreateAcc.css'
 import InputField from '../components/Input';
 import Label from '../components/Label'
-import { Eye, EyeOff } from 'lucide-react';
+import { Eye, EyeOff, Underline } from 'lucide-react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../components/Button';
@@ -23,24 +23,24 @@ function CreateAccountPage() {
                 <div className='container-createaccount'>
                     <h1 className='createaccountheading'> Make your first account </h1>
                     <br></br>
-                    <Label text="email address" className="createaccountlabel"/>
+                    <Label text="Email address" className="createaccountlabel"/>
                     <InputField placeholder="Enter email address" id="enter-email"/>
                     <br></br>
 
                     <div className='password-input-wrapper'>
-                    <Label text="password" className="createaccountlabel"/>
+                    <Label text="Password" className="createaccountlabel"/>
                     <InputField placeholder="Enter password" id="enter-password-field" type={showpassword ? "text" : "password"}/>
                     {showpassword ? <Eye className="icon-eye-password" onClick={passwordToggle}/> : <EyeOff className="icon-eye-password" onClick={passwordToggle}/>} </div>
                     <br></br>
 
                     <div className='password-input-wrapper'>
-                    <Label text="confirm-password" className="createaccountlabel"/>
+                    <Label text="Confirm password" className="createaccountlabel"/>
                     <InputField placeholder="confirm password" id="confirm-password-field" type={showPasswordConfirm ? "text" : "password"} />
                     {showPasswordConfirm ? <Eye className="icon-eye-password" onClick={passwordToggleConfirm} /> : <EyeOff className="icon-eye-password" onClick={passwordToggleConfirm} />} </div>
                     <br></br>
 
                     <Button text="confirm" className="createaccountbutton"/>
-                    <p> already have an account? <Link to={'/login'}> login </Link></p>
+                    <p className='loginpage'> Already have an account? <Link to={'/login'}> login </Link></p>
                 </div>
             </div>
         </> 
