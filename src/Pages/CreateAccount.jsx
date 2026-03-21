8f58debf832bd9c38f390a3c70fcbf1617afd738
David <daveedtee5@gmail.com>
2026-03-20 23:14:44 -0400

    connecting to the backend port, will be working on the backend repo

--- src/Pages/CreateAccount.jsx
+++ src/Pages/CreateAccount.jsx
@@ -1,72 +1,115 @@
-import '../styles/CreateAcc.css'
-import InputField from '../components/Input';
-import Label from '../components/Label'
-import { Eye, EyeOff, Underline } from 'lucide-react';
-import { useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Button from '../components/Button';
-import { isMatchingPassword } from '../utils/Logic';
+import "../styles/CreateAcc.css";
+import InputField from "../components/Input";
+import Label from "../components/Label";
+import { Eye, EyeOff, Underline } from "lucide-react";
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import Button from "../components/Button";
+import { isMatchingPassword } from "../utils/Logic";
 
 function CreateAccountPage() {
-    const [showpassword, setShowPassword] = useState(false);
-    const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
+	const [showpassword, setShowPassword] = useState(false);
+	const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
 
-    const emailref = useRef(null);
-    const passwordref = useRef(null);
-    const confirmpasswordref = useRef(null);
+	const emailref = useRef(null);
+	const passwordref = useRef(null);
+	const confirmpasswordref = useRef(null);
 
-    function passwordToggle() {
-        setShowPassword(!showpassword)
-    }
+	function passwordToggle() {
+		setShowPassword(!showpassword);
+	}
 
-    function passwordToggleConfirm() {
-        setShowPasswordConfirm(!showPasswordConfirm);
-    }
+	function passwordToggleConfirm() {
+		setShowPasswordConfirm(!showPasswordConfirm);
+	}
 
-    function showPasswordError() {
-        
-    }
+	function showPasswordError() {}
 
-    async function createAccount() {
-        const emailvalue = emailref.current.value;
-        const passwordvalue = passwordref.current.value;
-        const confirmpasswordvalue = confirmpasswordref.current.value;
-        console.log(emailvalue, passwordvalue, confirmpasswordvalue);
+	async function createAccount() {
+		const emailvalue = emailref.current.value;
+		const passwordvalue = passwordref.current.value;
+		const confirmpasswordvalue = confirmpasswordref.current.value;
+		console.log(emailvalue, passwordvalue, confirmpasswordvalue);
 
-        if (isMatchingPassword(passwordvalue, confirmpasswordvalue)){
-            console.log(true)
-        } else {
-            console.log(false)
-        }
-    }
-    return ( 
-        <>
-            <div className='container-container-createaccount'>
-                <div className='container-createaccount'>
-                    <h1 className='createaccountheading'> Make your first account </h1>
-                    <br></br> <br></br>
-                    <Label text="Email address" className="createaccountlabel"/>
-                    <InputField placeholder="Enter email address" id="enter-email" ref={emailref}/>
-                    <br></br>
+		if (isMatchingPassword(passwordvalue, confirmpasswordvalue)) {
+			console.log(true);
+		} else {
+			console.log(false);
+		}
+	}
 
-                    <div className='password-input-wrapper'>
-                    <Label text="Password" className="createaccountlabel"/>
-                        <InputField placeholder="Enter password" id="enter-password-field" type={showpassword ? "text" : "password"} ref={passwordref} />
-                    {showpassword ? <Eye className="icon-eye-password" onClick={passwordToggle}/> : <EyeOff className="icon-eye-password" onClick={passwordToggle}/>} </div>
-                    <br></br>
+	async function fetchData() {
+		try {
+			const backendport = import.meta.env.VITE_API_BASE_URL;
+			const response = await fetch(`${backendport}/api/response`);
 
-                    <div className='password-input-wrapper'>
-                    <Label text="Confirm password" className="createaccountlabel"/>
-                        <InputField placeholder="confirm password" id="confirm-password-field" type={showPasswordConfirm ? "text" : "password"} ref={confirmpasswordref} />
-                    {showPasswordConfirm ? <Eye className="icon-eye-password" onClick={passwordToggleConfirm} /> : <EyeOff className="icon-eye-password" onClick={passwordToggleConfirm} />} </div>
-                    <br></br>
+			if (response.ok) {
+				const data = await response.text();
+				console.log(data);
+			} else {
+				console.error(response.status);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	}
 
-                    <Button text="confirm" className="createaccountbutton" onClick={createAccount}/>
-                    <br></br>
-                    <p className='loginpage'> Already have an account? <Link to={'/login'}> login </Link></p>
-                </div>
-            </div>
-        </> 
-);}
+	fetchData();
+
+	return (
+		<>
+			<div className="container-container-createaccount">
+				<div className="container-createaccount">
+					<h1 className="createaccountheading"> Make your first account </h1>
+					<br></br> <br></br>
+					<Label text="Email address" className="createaccountlabel" />
+					<InputField
+						placeholder="Enter email address"
+						id="enter-email"
+						ref={emailref}
+					/>
+					<br></br>
+					<div className="password-input-wrapper">
+						<Label text="Password" className="createaccountlabel" />
+						<InputField
+							placeholder="Enter password"
+							id="enter-password-field"
+							type={showpassword ? "text" : "password"}
+							ref={passwordref}
+						/>
+						{showpassword ? (<Eye className="icon-eye-password" onClick={passwordToggle} />) : (<EyeOff className="icon-eye-password" onClick={passwordToggle} />)}
+					</div>
+					<br></br>
+					<div className="password-input-wrapper">
+						<Label text="Confirm password" className="createaccountlabel" />
+						<InputField
+							placeholder="confirm password"
+							id="confirm-password-field"
+							type={showPasswordConfirm ? "text" : "password"}
+							ref={confirmpasswordref}
+						/>
+						{showPasswordConfirm ? (
+							<Eye
+								className="icon-eye-password"
+								onClick={passwordToggleConfirm}
+							/>
+						) : (
+							<EyeOff
+								className="icon-eye-password"
+								onClick={passwordToggleConfirm}
+							/>
+						)}
+					</div>
+					<br></br>
+					<Button text="confirm" className="createaccountbutton" onClick={createAccount}/>
+					<br></br>
+					<p className="loginpage">
+						Already have an account? <Link to={"/login"}> login </Link>
+					</p>
+				</div>
+			</div>
+		</>
+	);
+}
 
 export default CreateAccountPage;
